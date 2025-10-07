1a8b5766a61eb52fd48333c9dc534d977584dbd2
Anirban Biswas <anirbanbofficial@gmail.com>
2025-10-07 11:28:41 +0530

    Schema Update 1.1

--- src/main.jsx
+++ src/main.jsx
@@ -56,43 +56,43 @@ const router = createBrowserRouter(
   )
 )
 
-const websiteSchema = {
-  "@context": "https://schema.org",
-  "@type": "WebSite",
-  "name": "Global Professional Certifications",
-  "url": "https://globalprofessionalcertifications.com",
-  "potentialAction": {
-    "@type": "SearchAction",
-    "target": "https://globalprofessionalcertifications.com/search?query={search_term_string}",
-    "query-input": "required name=search_term_string"
-  }
-};
+// const websiteSchema = {
+//   "@context": "https://schema.org",
+//   "@type": "WebSite",
+//   "name": "Global Professional Certifications",
+//   "url": "https://globalprofessionalcertifications.com",
+//   "potentialAction": {
+//     "@type": "SearchAction",
+//     "target": "https://globalprofessionalcertifications.com/search?query={search_term_string}",
+//     "query-input": "required name=search_term_string"
+//   }
+// };
 
-const organizationSchema = {
-  "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "Global Professional Certifications",
-  "url": "https://globalprofessionalcertifications.com",
-  "logo": "https://globalprofessionalcertifications.com/logo.png",
-  "sameAs": [
-    "https://www.linkedin.com/company/global-professional-certifications/",
-    "https://www.instagram.com/global__professionals",
-    "https://www.youtube.com/watch?v=2FWaO_Cf0eg"
-  ]
-};
+// const organizationSchema = {
+//   "@context": "https://schema.org",
+//   "@type": "Organization",
+//   "name": "Global Professional Certifications",
+//   "url": "https://globalprofessionalcertifications.com",
+//   "logo": "https://globalprofessionalcertifications.com/logo.png",
+//   "sameAs": [
+//     "https://www.linkedin.com/company/global-professional-certifications/",
+//     "https://www.instagram.com/global__professionals",
+//     "https://www.youtube.com/watch?v=2FWaO_Cf0eg"
+//   ]
+// };
 
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <HelmetProvider>
       <>
-        <Helmet>
+        {/* <Helmet>
           <script type="application/ld+json">
             {JSON.stringify(websiteSchema)}
           </script>
           <script type="application/ld+json">
             {JSON.stringify(organizationSchema)}
           </script>
-        </Helmet>
+        </Helmet> */}
         <RouterProvider router={router} />
       </>
     </HelmetProvider>
