f0f2bda2721905ae207db432f1d0cf4387bf0c03
Anirban <anirbanbofficial@gmail.com>
2026-08-18 11:56:37 +0530

    Fix: Contact Cards

--- src/components/Contact/ContactUs.jsx
+++ src/components/Contact/ContactUs.jsx
@@ -179,7 +179,7 @@ export default function ContactUs() {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
                     {/* Heading */}
-                    <div className="text-center max-w-2xl mx-auto mb-14">
+                    <div className="text-center max-w-3xl mx-auto mb-14">
                         <h2 className="text-2xl md:text-4xl font-bold text-gray-900 mb-4">
                             Send Us a <span className="text-brand-blue font-normal italic">Message</span>
                         </h2>
